2240c45b5e9e99940448480ebefe729bd978eab4
Syed Bilal Hussain <syedbilalhussain168@gmail.com>
2017-09-17 22:23:35 +0500

    Update

--- exercises.js
+++ exercises.js
@@ -2,7 +2,10 @@
 
 function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
-  // if they are the same return either one
+  if(x>=y)
+	return x;
+  else
+	return y;
 }
 
 function greeting(language) {
@@ -11,18 +14,36 @@ function greeting(language) {
   // language: 'English' -> 'Hello!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
+  if(language=='German')
+	return 'Guten Tag!';
+  else if(language=='Spanish')
+	return 'Hola!';
+  else
+	return 'Hello!';
 }
 
 function isTenOrFive(num) {
   // return true if num is 10 or 5
   // otherwise return false
+  if(num==10||num==5)
+	return true;
+  else
+	return false;
 }
 
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
+  if(num<50&&num>20)
+	return true;
+  else
+	return false;
 }
 
 function isInteger(num) {
+  if(num==Math.floor(num)&&num==Math.round(num))
+	return true;
+  else
+	return false;
   // return true if num is an integer
   // 0.8 -> false
   // 1 -> true
@@ -36,6 +57,14 @@ function fizzBuzz(num) {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  if(num%3==0&&num%5==0)
+	return 'fizzbuzz';
+  else if(num%3==0)
+	return 'fizz';
+  else if(num%5==0)
+	return 'buzz';
+  else
+	return num;
 }
 
 function isPrime(num) {
@@ -44,35 +73,56 @@ function isPrime(num) {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+  if(num==0||num==1)
+	return false;
+  else {
+	for(var i=2;i<num;i++) {
+		if(num%i==0)
+			return false;
+	}
+	return true;
+  }
 }
 
 function returnFirst(arr) {
   // return the first item from the array
+  return arr[0];
 }
 
 function returnLast(arr) {
   // return the last item of the array
+  return arr[arr.length-1];
 }
 
 function getArrayLength(arr) {
   // return the length of the array
+  return arr.length;
 }
 
 function incrementByOne(arr) {
   // arr is an array of integers  
   // increase each integer by one
   // return the array
+  var ans=[];
+  for(var i=0;i<arr.length;i++) {
+	ans[i]=arr[i]+1;
+  }
+  return ans;
 }
 
 function addItemToArray(arr, item) {
   // add the item to the end of the array
   // return the array
+  arr.push(item);
+  return arr;
 }
 
 function addItemToFront(arr, item) {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+  arr.unshift(item);
+  return arr;
 }
 
 function wordsToSentence(words) {
@@ -80,26 +130,55 @@ function wordsToSentence(words) {
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
+  var str='';
+  for(var i=0;i<words.length;i++) {
+	str+=words[i];
+	if(i!=words.length-1)
+	str+=' ';
+  }
+  return str;
 }
 
 function contains(arr, item) {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
+  for(var i=0;i<arr.length;i++) {
+	if(arr[i]==item)
+		return true;
+  }
+  return false;
 }
 
 function addNumbers(numbers) {
   // numbers is an array of integers.
   // add all of the integers and return the value
+  var sum=0;
+  for(var i=0;i<numbers.length;i++) {
+	sum+=numbers[i];
+  }
+  return sum;
 }
 
 function averageTestScore(testScores) {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+  var avg=0,sum=0;
+  for(var i=0;i<testScores.length;i++) {
+	sum+=testScores[i];
+  }
+  avg=sum/testScores.length;
+  return avg;
 }
 
 function largestNumber(numbers) {
   // numbers is an array of integers
   // return the largest integer
+  var l=numbers.length,a=numbers[0];
+  for(var i=0;i<l;i++) {
+	if(numbers[i]>a)
+		a=numbers[i];
+  }
+  return a;
 }
 
 // Do not modify code below this line.
